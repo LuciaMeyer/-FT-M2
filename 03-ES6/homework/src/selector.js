50dad2f39f9e2c0ba62aef923db239b2a5c48b2d
Lucia Meyer <luciameyer1603@gmail.com>
2022-04-26 22:27:46 -0300

    homework

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -1,49 +1,115 @@
-var traverseDomAndCollectElements = function(matchFunc, startEl) {
-  var resultSet = [];
 
-  if (typeof startEl === "undefined") {
-    startEl = document.body;
-  }
+/* OBJETIVO DE CADA FUNCIÓN
 
-  // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
-  // usa matchFunc para identificar elementos que matchien
+1) el usuario llama a $ para seleccionar elementos de su webpage según un selector específico.
+2) $ llama a matchFunctionMaker que usa a selectorTypeMatcher q identifica el tipo de selector seleccionado 
+- selectorTypeMatcher('.classOne') --> es una 'class'
+- matchFunctionMaker crea una función que devuelve true si lo encuentra
+- matchFunctionMaker('.classOne') --> true (está la clase en el HTML)
+3) traverseDomAndCollectElements recorre el DOM y devuelve un array con los elementos HTML de esa clase
 
-  // TU CÓDIGO AQUÍ
-  
-};
-
-// Detecta y devuelve el tipo de selector
-// devuelve uno de estos tipos: id, class, tag.class, tag
-
-
-var selectorTypeMatcher = function(selector) {
-  // tu código aquí
-  
-};
-
-// NOTA SOBRE LA FUNCIÓN MATCH
-// recuerda, la función matchFunction devuelta toma un elemento como un
-// parametro y devuelve true/false dependiendo si el elemento
-// matchea el selector.
-
-var matchFunctionMaker = function(selector) {
-  var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
-  if (selectorType === "id") { 
-   
-  } else if (selectorType === "class") {
-    
-  } else if (selectorType === "tag.class") {
-    
-  } else if (selectorType === "tag") {
-    
-  }
-  return matchFunction;
-};
-
+/*________________________________________________________________________________________
+EXPLICACIÓN LU:
+el usuario llama a $ para seleccionar elementos de su webpage según un selector específico.
+*/
 var $ = function(selector) {
   var elements;
   var selectorMatchFunc = matchFunctionMaker(selector);
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
+
+/*________________________________________________________________________________________
+EXPLICACIÓN LU:
+la función recibe un selector (css) y devuelve una función, una "maquina de encontrar"
+la función "maquina" recibe un elemento html y lo compara con el selector,
+retorna true si lo encuentra o false si no lo encuentra.
+Dependiendo de qué selector sea entra a cada if
+en el caso de .classOne entra al if de selectorType === "class"
+busca en cada nodo del HTML una clase con el nombre del selector .classOne
+si lo encuentra retorna true */ 
+
+function matchFunctionMaker(selector) {
+
+  var selectorType = selectorTypeMatcher(selector);
+  var matchFunction;
+
+  if (selectorType === "id") {
+    matchFunction = (eleHTML) => '#' + eleHTML.id === selector;
+  }
+  if (selectorType === "class") {
+    matchFunction = function (eleHTML) {
+      let arrClass = eleHTML.classList; // como puede tener + de 1 clase uso class list q devuelve un array de las clases     
+      for (let i = 0; i < arrClass.length; i++) {
+        if ('.' + arrClass[i] === selector)
+          return true;
+      }
+      return false;
+    };
+  }
+  if (selectorType === "tag.class") { // ej: div.classOne
+    matchFunction = function (eleHTML) {
+      let [t, c] = selector.split('.'); // split --> ['div', 'classOne'] guardo uno en cada variable t y c      
+      return matchFunctionMaker(t)(eleHTML) && matchFunctionMaker('.' + c)(eleHTML);
+    };
+    // matchFunctionMaker(t) devuelve la función nmatchFunction por eso la vuelvo a invocar con ese elemento
+    // para saber si ese tag existe en el HTML, retorna true
+  }
+  if (selectorType === "tag") {
+    matchFunction = function (eleHTML) {
+      return eleHTML.localName === selector;
+    };
+  }
+  return matchFunction; // devuelvo la función (maquina de encontrar)     
+}
+
+/*________________________________________________________________________________________
+EXPLICACIÓN LU:
+la función recibe un string del selector (css) como los ej de abajo
+y devuelve un string que determina qué tipo de selector es
+RECIBE --> RETORNA
+'#hola'--> 'id'  
+'.hola' --> 'class'
+'hola' --> 'tag'
+'hola.hola' --> 'tag class' */
+
+function selectorTypeMatcher(selector) {
+  // tu código aquí
+  if (selector[0] === '#')
+    return 'id';
+  if (selector[0] === '.')
+    return 'class';
+  if (selector.split('.').length > 1)
+    return 'tag.class';
+  return 'tag';
+}
+
+/*________________________________________________________________________________________
+EXPLICACIÓN LU:
+Esta función recibe una función matchFunc que es la misma que la función 2:
+busca la clase en los elementos y devuelve true or false si están
+y recibe como 2do parámetro un elemento incial startEl.
+Si el elemento que estoy recorriendo tiene la calse (matchFunc es true)
+entonces pusheo el elemento dentro de resultSet y aplico recursión a los hijos
+importante ir concatenando cada resultSet de las recursiones
+*/
+
+function traverseDomAndCollectElements(matchFunc, startEl) {
+  var resultSet = [];
+
+  if (typeof startEl === "undefined")
+    startEl = document.body;
+  if (matchFunc(startEl))
+    resultSet.push(startEl);
+
+  for (let i = 0; i < startEl.children.length; i++) {
+    let child = startEl.children[i];
+    let eFounds = traverseDomAndCollectElements(matchFunc, child);
+    resultSet = [...resultSet, ...eFounds];
+  }
+  return resultSet;
+}
+
+
+
+
