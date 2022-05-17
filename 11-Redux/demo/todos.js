56e149ab0cdb5b094724aabfab40f7bf11293853
LuciaM <luciameyer1603gmail.com>
2022-05-17 17:02:59 -0300

    hw redux

--- 11-Redux/demo/todos.js
+++ 11-Redux/demo/todos.js
@@ -1,15 +1,16 @@
-const redux = require('redux');
+const { createStore } = require('redux');
 
-const createStore = redux.createStore;
+// const createStore = redux.createStore;
 
+// creo mis constantes 
 const ADD_TODO = 'ADD_TODO'
 const REMOVE_TODO = 'REMOVE_TODO';
-
-
 const initialState = {
   todos: []
 }
 
+
+// creo el reducer
 const rootReducer = (state = initialState, action) => {
   switch(action.type) {
     case ADD_TODO:
@@ -25,29 +26,31 @@ const rootReducer = (state = initialState, action) => {
   }
 }
 
+// creo el store diciendolé qué reducer usar
 const store = createStore(rootReducer);
 
+// mi archivo de js se subscribe al store con este método
 store.subscribe(() => {
   console.log('Subscription: ', store.getState());
 });
 
+// tengo mi action creator
 function addTodo(text) {
   return {
     type: ADD_TODO,
     payload: text
   }
 }
-
+// tengo otra action creator
 function removeTodo(index) {
   return {
     type: REMOVE_TODO,
     payload: index
   }
 }
-
+// despacho las actions
 store.dispatch(addTodo('Comprar pan'))
 store.dispatch(addTodo('Correr'))
-
 store.dispatch(removeTodo(1))
-
+// pregunto por el nuevo estado
 console.log(store.getState());
